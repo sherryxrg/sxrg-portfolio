7011d7eb7068cc50994f3c61d363b7c3dda3e9a1
Sherry <sherryxrg@outlook.com>
2020-03-14 22:59:04 -0700

    carousel implemented with swiper.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.log("page loaded")
 
+var swiper = new Swiper('.swiper-container', {
+  cssMode: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  mousewheel: true,
+  keyboard: true,
+});
+
