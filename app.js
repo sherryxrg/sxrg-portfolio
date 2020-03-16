5e61b74a81a2a6aab945844e84f44c9b26bb19e5
Sherry <sherryxrg@outlook.com>
2020-03-16 16:52:28 -0700

    show/hide navbar now working

--- app.js
+++ app.js
@@ -1,5 +1,64 @@
+// logs
 console.log("page loaded")
+window.onload = function() {
+    if (window.jQuery) {  
+        console.log("JQuery loaded.");
+    } else {
+        console.log("Doesn't Work");
+    }
+}
 
+// -- hide/show navbar only on scroll
+//document.getElementById("navigation").style.display = 'none'; -- hides navbar
+
+$.fn.isInViewport = function () {
+    // getting boundaries of element
+    var elemTop = $(this).offset().top;
+    var elemBottom = elemTop + $(this).outerHeight();
+
+    // getting boundaries of viewport
+    var vpTop = $(window).scrollTop();
+    var vpBottom = vpTop + $(window).height();
+
+    // check if element is inside 
+    return elemBottom > vpTop && elemTop < vpBottom;
+};
+
+// implementation
+var navbar = $('#navigation')
+navbar.hide();
+
+$('.wrapper').on('resize scroll', function() {
+    
+    // hide the navbar if section == #title
+    if (!$('#title').isInViewport()) {
+        navbar.fadeIn(); 
+    } else {
+        navbar.fadeOut();
+    }
+
+    // highlight current section 
+    if ($('#projects').isInViewport()) {
+        $('#navProjects').addClass('nav-active');
+
+        $('#navMockups').removeClass('nav-active');
+        $('#navAbout').removeClass('nav-active');
+    } else if ($('#mockups').isInViewport()) {
+        $('#navMockups').addClass('nav-active');
+
+        $('#navProjects').removeClass('nav-active');
+        $('#navAbout').removeClass('nav-active');
+    } else if ($('#about').isInViewport()) {
+        $('#navAbout').addClass('nav-active');
+
+        $('#navMockups').removeClass('nav-active');
+        $('#navProjects').removeClass('nav-active');
+    }
+
+});
+
+
+// -- swiper.js 
 var swiper = new Swiper('.swiper-container', {
   cssMode: true,
   navigation: {
