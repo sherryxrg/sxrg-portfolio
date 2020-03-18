f2bc2a65f136fef0dc5856314e34d3ab492cedbd
Sherry <sherryxrg@outlook.com>
2020-03-18 12:10:06 -0700

    added barchart showing skills

---
+++ charts.js
@@ -0,0 +1,56 @@
+
+console.log("chartjs loaded");
+
+var boxframe = $('#barchart');
+console.log(boxframe.height());
+console.log(boxframe.width());
+var c = document.getElementById("barchart");
+var ctx = c.getContext("2d");
+
+// canvas is showing
+// ctx.beginPath();
+// ctx.rect(0, 0, 500, 500);
+// ctx.fillStyle = "green";
+// ctx.fill();
+
+var data = {
+    labels: ["HTML5", "CSS+SASS", "JS/jQuery", "React.js", "Node.js", "Java", "Python 3"],
+    datasets: [{
+            backgroundColor: "white",
+            hoverBackgroundColor: "rgba(255,255,255,0.7)",
+            data: [100, 90, 80, 70, 55, 85, 85]
+        }]
+};
+
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: { 
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{
+                gridLines: {
+                    display:false
+                },
+                ticks: {
+                    display: false,
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [{
+                barPercentage: 0.4,
+                gridLines: {
+                    display:false
+                },
+                ticks: {
+                    fontColor: "white",
+                    fontFamily: 'Roboto Mono',
+                    fontSize: 11,
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+})
